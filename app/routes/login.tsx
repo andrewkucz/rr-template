7b9c80516f2743c515dd01f0204498b1a5d53976
Andrew Kuczynski <kuczynskiandrew@gmail.com>
2026-01-09 12:48:13 -0500

    login page + button

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,3 +1,36 @@
-export default function Login() {
-	return <div>Login</div>;
+import { useMutation } from "@tanstack/react-query";
+import { redirect } from "react-router";
+import { Button } from "~/components/ui/button";
+import { authClient } from "~/lib/auth/browser";
+import { auth } from "~/lib/auth/server";
+import type { Route } from "./+types/login";
+
+export const loader = async ({ request }: Route.LoaderArgs) => {
+	const session = await auth.api.getSession({
+		headers: request.headers,
+	});
+	if (session) {
+		throw redirect("/");
+	}
+};
+
+export default function LoginPage() {
+	const { mutate: login } = useMutation({
+		mutationFn: async () => {
+			const result = await authClient.signIn.social({ provider: "discord" });
+			if (result.error) {
+				throw result.error;
+			}
+			return result.data;
+		},
+		onError(error) {
+			console.error(error);
+		},
+	});
+
+	return (
+		<div>
+			<Button onClick={() => login()}>Login with Discord</Button>
+		</div>
+	);
 }
